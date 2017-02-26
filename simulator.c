eed704885cd56aebf04d2f76f1409d97ac9db5c8
Connor Grehlinger <cgrehlin@udel.edu>
2017-02-26 15:48:21 +0000

    Created initial files for simulator.

---
+++ simulator.c
@@ -0,0 +1,13 @@
+/*
+Connor Grehlinger, CPEG324
+C source file for ISA simulator 
+
+Task 2: (Individual Work) Implement the ISA in a simulator written in C 
+(or in another programming language of choice). The simulator must accept 
+a file that encodes instructions of your ISA in binary (you can use a 
+textual representation of the binary code, e.g., “00110010” in the file, 
+or use the true binary file format), and print out the result. 
+The simulator shouldn’t accept any other input.
+*/
+
+#include "simulator.h"
---
+++ simulator.h
@@ -0,0 +1,6 @@
+// Connor Grehlinger, CPEG324
+// Header file for ISA simulator 
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
